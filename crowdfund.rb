8c51eeeb9fa7b7cfd86849ed97d647bf2f95b48d
Mario Solomou <webmaster@masodev.com>
2014-07-10 10:53:26 +0100

    Crowdfund
    Used one Array / For each loop
    use single and double-quoted strings
    concatenate strings
    mix strings and numbers
    use escape sequences
    evaluate (interpolate) Ruby code within a double-quoted string using #{}

---
+++ crowdfund.rb
@@ -0,0 +1,11 @@
+projects = ['Project ABC','Project LMN','Project XYZ']
+
+fundingProject1 = 1000
+
+
+puts "Projects have a total funding of #{fundingProject1}"
+
+puts "Projects:"
+projects.each do |project|
+puts "\n\t#{project}"
+end
