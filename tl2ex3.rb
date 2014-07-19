7ccefbf8cdd7095bc6ab89b41b71e8b81dd0a459
Mario Solomou <webmaster@masodev.com>
2014-07-20 00:55:59 +0300

    Variables & Methods

---
+++ tl2ex3.rb
@@ -0,0 +1,9 @@
+# Add another section that prints the name of the user 10 times. You must do this without explicitly writing the puts method 10 times in a row.
+
+name = "Mario"
+
+10.times do
+
+puts name
+
+end
---
+++ tl2ex4.rb
@@ -0,0 +1,8 @@
+# Modify name.rb again so that it first asks the user for their first name, saves it into a variable, and then does the same for the last name. Then outputs their full name all at once.
+
+
+puts "What is your first name?"
+first_name = gets.chomp
+puts "Thank you. What is your last name?"
+last_name = gets.chomp
+puts "Great. So your full name is " + first_name + " " + last_name
